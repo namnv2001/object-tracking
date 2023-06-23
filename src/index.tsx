e5fbc262ac46af300b6f3bb579b07f0c26897a38
namnv2001 <namnv.forwork@gmail.com>
2023-06-23 22:06:55 +0700

    setup route, 404 page

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from 'routes/Home';
+import Metric from 'routes/Metric';
+import NotFoundPage from 'routes/NotFoundPage';
+import 'styles/index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: '/metric',
+        element: <Metric />,
+      },
+    ],
+  },
+]);
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container as HTMLElement);
 
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
---
+++ src/routes/Home.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+const Root = () => {
+  return (
+    <div id="sidebar" className="min-h-full">
+      <nav className="bg-gray-800">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-8 w-8"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Object tracking"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <Link
+                    to="/"
+                    className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                    aria-current="page"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/metric"
+                    className="hover:bg-gray-700 text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Metric
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Root;
