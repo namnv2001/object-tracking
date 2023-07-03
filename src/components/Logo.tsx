eeac98c56aca113d615e241e4370fbae289c8bf0
namnv2001 <namnv.forwork@gmail.com>
2023-07-03 22:21:52 +0700

    refactor, add ConfigProvider

---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import { useMqttContext } from 'context';
+import React from 'react';
+
+const Logo = () => {
+  const { isSubscribed } = useMqttContext();
+  return (
+    <div className="flex-shrink-0">
+      <div
+        className={`w-8 h-8 rounded-full ${
+          isSubscribed ? 'bg-green-500 animate-pulse' : 'bg-gray-500'
+        }`}
+      />
+    </div>
+  );
+};
+
+export default Logo;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,37 +1,47 @@
+import { Layout } from 'antd';
 import LinkItem from 'components/LinkItem';
+import Logo from 'components/Logo';
 import { path } from 'constants/common';
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-
-const Logo = () => {
-  return (
-    <div className="flex-shrink-0">
-      <div className="w-8 h-8 bg-green-500 rounded-full animate-pulse" />
-    </div>
-  );
-};
+import { Outlet, useLocation } from 'react-router-dom';
 
 const Home = () => {
+  const location = useLocation();
   return (
     <div id="sidebar" className="min-h-full">
       <nav className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <Layout className="bg-gray-800 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="flex items-center">
               <Logo />
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   {path.map((item, index) => (
-                    <LinkItem path={item.path} name={item.name} key={index} />
+                    <LinkItem
+                      path={item.path}
+                      location={location.pathname}
+                      name={item.name}
+                      key={index}
+                    />
                   ))}
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Layout>
       </nav>
       <div id="detail">
-        <Outlet />
+        <Layout className="mx-auto max-w-7xl h-screen sm:px-6 lg:px-8">
+          <div className="mt-10">
+            {location.pathname !== '/' ? (
+              <Outlet />
+            ) : (
+              <div className="text-2xl">
+                Welcome to object tracking application
+              </div>
+            )}
+          </div>
+        </Layout>
       </div>
     </div>
   );
