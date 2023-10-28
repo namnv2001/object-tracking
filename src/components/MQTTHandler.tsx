704e07a0d7aa8be0b1511db94d969bef7ad38f9c
namnv2001 <namnv.forwork@gmail.com>
2023-10-28 21:08:56 +0700

    update

--- src/components/MQTTHandler.tsx
+++ src/components/MQTTHandler.tsx
@@ -72,7 +72,6 @@ const MQTTHandler = () => {
     const vertical = parseFloat(y);
     const horizontal = parseFloat(x);
     const timestamp = Math.floor(getTime(new Date()) / 1000); // seconds
-    console.log(timestamp);
     const purifiedData = {
       vertical,
       horizontal,
